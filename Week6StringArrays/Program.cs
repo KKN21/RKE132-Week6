b1d3a24beaa9f274f8ecc705250cb36366ce8990
KKN21 <karlkenneth.neuhaus@gmail.com>
2024-04-25 13:30:17 +0300

    Week 6

---
+++ Week6StringArrays/Program.cs
@@ -0,0 +1,24 @@
+﻿string hello = " hello, world! Here I am!";
+
+int stringLength = hello.Length;
+
+
+hello = hello.Trim();
+
+char firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+
+hello = hello.Replace(hello[0], Char.ToUpper(hello[0])).Replace('?', '!');
+
+firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+
+//int wordCounter = 1;
+//for (int i = 0; i < hello.Length; i++)
+//{
+//    if (char.IsWhiteSpace(hello[i]))
+//    {
+//        wordCounter++;
+//    }
+//}
+//Console.WriteLine($"Word count in ({hello}): {wordCounter} words.");
